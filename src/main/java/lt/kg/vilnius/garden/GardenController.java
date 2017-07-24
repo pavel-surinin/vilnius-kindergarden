cb4b433440a4a30cca460d050185cb7f354bbcb8
pavel-surinin <pavel.surinin@gmail.com>
2017-07-24 20:17:41 +0300

    added cors

--- src/main/java/lt/kg/vilnius/garden/GardenController.java
+++ src/main/java/lt/kg/vilnius/garden/GardenController.java
@@ -13,28 +13,33 @@ public class GardenController {
     @Autowired
     GardenService service;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("api/garden")
     public Iterable<GardenEntity> findAll(){
         return service.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("api/garden/{id}")
     public GardenEntity findById(@PathVariable Long id){
         return service.findById(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("api/garden")
     public GardenEntity save(@RequestBody GardenEntity garden){
         return service.save(garden);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("api/garden")
     public void deleteAll(){
         service.deleteAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @ResponseStatus(HttpStatus.ACCEPTED)
     @PostMapping("api/garden/{id}")
     public KidsGroup save(@RequestBody KidsGroup group, @PathVariable Long id){
